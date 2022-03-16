9804baec25e53efcbc2690a8e6864ae4bc1f082e
Rizki Pandiwa <pandiwarizki.rp@gmail.com>
2022-03-16 09:59:47 +0700

    fix port

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const NotesService = require("./services/inMemory/NotesService");
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: process.env.PORT,
+    port: 5000,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
